3b34f81f96a5a77d46bb5f7648a2e57c230952bd
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-14 10:12:24 +0530

    Longest subarray with sum k(including negative numbers)

--- Array/Traversal/LongestSubArrayWithSumK.java
+++ Array/Traversal/LongestSubArrayWithSumK.java
@@ -1,26 +1,35 @@
 import java.util.*;
 
 public class LongestSubArrayWithSumK {
-    public static int longestSubarrayWithSumK(int[] a, long k) {
-        int ans = 0;
-        int n = a.length;
-        for (int i = 0; i < n; i++) {
-            int temp = a[i];
-            for (int j = i + 1; j < n; j++) {
-                if (temp == k) {
-                    ans = Math.max(ans, j - i);
-                    break;
-                }
-                temp += a[j];
-            }
-        }
 
-        return ans;
-    }
+  public static int longestSubarrayWithSumK(int[] nums, long k) {
+    int n = nums.length;
+    int i = 0;
+    int j = 0;
+    int maxLen = -1;
+    int sum = 0;
 
-    public static void main(String[] args) {
-        int[] a = { 1, 2,3,1,1,1,1 };
-        int k = 3;
-        System.out.println(longestSubarrayWithSumK(a, k));
+    while (j < n) {
+      sum += nums[j];
+      if (sum == k) {
+        maxLen = Math.max(maxLen, j - i + 1);
+        j++;
+      } else if (sum < k) {
+        j++;
+      } else {
+		while (sum > k) {
+		  sum -= nums[i];
+		  i++;
+		}
+		j++;
+      }
     }
+    return maxLen;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 3, 3 };
+    int k = 6;
+    System.out.println(longestSubarrayWithSumK(nums, k));
+  }
 }
