e98ee48cd5c55aecc4a85f1552cea415f990bd5a
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-23 16:51:02 +0530

    Single number in sorted array

---
+++ Binary Search/SingleNumer.java
@@ -0,0 +1,35 @@
+public class SingleNumer {
+
+  public int singleNonDuplicate(int[] nums) {
+    int n = nums.length;
+    if (n == 1) return nums[0];
+    if (nums[0] != nums[1]) return nums[0];
+    if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+    int low = 1;
+    int high = n - 2;
+    int mid = (low + high) / 2;
+
+    while (low <= high) {
+      if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+        return nums[mid];
+      } else if (
+        (mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
+        (mid % 2 == 0 && nums[mid] == nums[mid + 1])
+      ) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+
+      mid = (low + high) / 2;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+    SingleNumer s = new SingleNumer();
+    System.out.println(s.singleNonDuplicate(arr));
+  }
+}
